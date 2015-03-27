54c7df11aaec37b913d940c1fac9f7cded3d6d06
Cedric Fung <cedric@vec.io>
2015-03-27 13:53:22 +0800

    create and destroy git repo with book callback

--- app/models/book.rb
+++ app/models/book.rb
@@ -1,2 +1,19 @@
 class Book < ActiveRecord::Base
+  belongs_to :user
+
+  def repo
+    path = "/tmp/repos/#{user.id}/#{self.id}"
+    git = Rugged::Repository.new(path) rescue Rugged::Repository.init_at(path)
+    git.workdir
+  rescue => e
+    puts e
+  end
+
+  after_create do
+    destroy unless repo
+  end
+
+  after_destroy do
+    FileUtils.rm_rf(repo)
+  end
 end
