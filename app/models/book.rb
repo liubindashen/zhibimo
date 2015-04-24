4d687f62b31849cfc79cf83afe8b1e68588ba3e7
hpyhacking <crazyflapjack@gmail.com>
2015-04-24 18:12:11 +0800

    oops !

--- app/models/book.rb
+++ app/models/book.rb
@@ -39,7 +39,7 @@ class Book < ActiveRecord::Base
         plugins: %w(tongji),
         pluginsConfig: {
           tongji: {
-            token: ENV['TONGJI-TOKEN']
+            token: ENV['TONGJI_TOKEN']
           }
         }
       })
