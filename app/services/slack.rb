0470c54ed55d541343a948e01c4f868196757d7f
hpyhacking <crazyflapjack@gmail.com>
2015-05-21 22:48:26 +0800

    disable logger

--- app/services/slack.rb
+++ app/services/slack.rb
@@ -2,7 +2,6 @@ class Slack
   def self.send(message) 
     conn = Faraday.new(:url => 'https://hooks.slack.com') do |faraday|
       faraday.request  :url_encoded             # form-encode POST params
-      faraday.response :logger                  # log requests to STDOUT
       faraday.adapter  Faraday.default_adapter  # make requests with Net::HTTP
     end
 
