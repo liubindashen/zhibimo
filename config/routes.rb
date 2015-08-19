dfab5e6dde3628eed8d77da51521b82c1c6c4531
hpyhacking <crazyflapjack@gmail.com>
2015-08-19 13:31:36 +0800

    refactor route

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,46 @@
 Rails.application.routes.draw do
-  ActiveAdmin.routes(self)
+  get '/signin' => 'welcome#new', as: :signin
+  get '/signout' => 'sessions#destroy', as: :signout
+  get '/auth/failure' => 'sessions#fail'
+  get '/auth/:provider/callback' => 'sessions#create'
+
+  get '/explore/books', to: 'books#index', as: :explore_books
+  get '/explore/authors', to: 'authors#index', as: :explore_authors
+
+  get '/books/:author', to: 'authors#show'
+  get '/books/:author/:slug', to: 'books#show'
+
+  namespace :reader do
+    resources :books, only: [:index] do
+      resources :donates, only: [:new, :show, :create] do
+        member do
+          post :callback
+        end
+      end
+      resources :purchases, only: [:show, :create] do
+        member do
+          post :callback
+        end
+      end
+    end
+  end
+
+  namespace :author do
+    resources :orders, only: [:index, :show]
+    resources :books, only: [:index, :edit, :update, :create, :new] do
+      resources :orders, only: [:index]
+
+      resource :desk, only: [:show]
+      resource :purchase, only: [:edit, :update]
+
+      resources :builds, only: [:update, :index] do
+        collection do
+          post 'hook'
+        end
+      end
+    end
+  end
+
   namespace :api do
     scope '/v1' do
       resources :books, only: [] do
@@ -14,40 +55,7 @@ Rails.application.routes.draw do
     end
   end
 
-  namespace :explore do
-    resources :books, only: [:index, :show] do
-      resources :donates, only: [:new, :show, :create] do
-        post :callback
-      end
-      resources :purchases, only: [:show, :create] do
-        post :callback
-      end
-    end
-    resources :downloads, only: [:show]
-    resources :authors, only: [:show]
-  end
-
-  resource :author, only: [:edit, :update, :new, :create]
-
-  resources :orders, only: [:index]
-
-  resources :books, only: [:index, :edit, :update, :create, :new] do
-
-    resource :desk, only: [:show]
-    resource :purchase, only: [:edit, :update]
-
-    resources :orders, only: [:index]
-    resources :builds, only: [:update, :index] do
-      collection do
-        post 'hook'
-      end
-    end
-  end
-
-  get '/signin' => 'welcome#new', as: :signin
-  get '/signout' => 'sessions#destroy', as: :signout
-  get '/auth/failure' => 'sessions#fail'
-  get '/auth/:provider/callback' => 'sessions#create'
+  ActiveAdmin.routes(self)
 
   match '/404', to: 'errors#file_not_found', via: :all
 
