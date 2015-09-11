a0c047014f33826cc46ee4333359dc50a17e870a
poppinyunhai <yiranyunhaisrh@gmail.com>
2015-09-11 18:04:04 +0800

    fix render methods

--- app/controllers/book/domain_bindings_controller.rb
+++ app/controllers/book/domain_bindings_controller.rb
@@ -1,6 +1,6 @@
 class Book::DomainBindingsController < ApplicationController
   def show
     @domain = DomainBinding.find_by(domain: requst.host)
-    @domain ? render "" : redirect_to root_path
+    @domain ? "" : redirect_to root_path
   end
 end
