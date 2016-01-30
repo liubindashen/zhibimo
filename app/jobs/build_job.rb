4159072153082cd82c01d7cf2674cb93860d481a
poppinyunhai <yiranyunhaisrh@gmail.com>
2016-01-30 23:21:21 +0800

    change rm -rf

--- app/jobs/build_job.rb
+++ app/jobs/build_job.rb
@@ -72,12 +72,11 @@ class BuildJob < ActiveJob::Base
       system("sed -i 's/\\s/ /g' #{commit_path}/*")
 
       # build html to books/author/book/release/commit/
-      cmd = system("gitbook build #{commit_path} #{release_path}/ --log=debug --debug")
-      Open3.popen3(cmd) do |stdin, stdout, stderr, wait_thr|
-        build.update(build_log: stdout.read)
-        #puts "stdout is:" + stdout.read
-        #puts "stderr is:" + stderr.read
-      end
+      #cmd =
+      system("gitbook build #{commit_path} #{release_path}/ --log=debug --debug")
+      # Open3.popen3(cmd) do |stdin, stdout, stderr, wait_thr|
+      #   build.update(build_log: stdout.read)
+      # end
       if book.free?
         # build pdf to books/author/book/release/commit/book.mobi
         mobi_output = "#{release_path}/#{book.slug}.mobi"
@@ -92,7 +91,7 @@ class BuildJob < ActiveJob::Base
         system("xvfb-run gitbook pdf #{commit_path} #{pdf_output}")
       end
 
-      system("rm -f #{current_path} && ln -sf #{release_path} #{current_path}")
+      system("rm -rf #{current_path} && ln -sf #{release_path} #{current_path}")
     end
   end
 end
