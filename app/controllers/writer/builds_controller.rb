6d8b49af67f90d2317553d981f5fcb07560d2ba4
poppinyunhai <yiranyunhaisrh@gmail.com>
2016-02-02 16:34:32 +0800

    add build form github

--- app/controllers/writer/builds_controller.rb
+++ app/controllers/writer/builds_controller.rb
@@ -19,10 +19,10 @@ module Writer
       redirect_to writer_book_builds_path(@book)
     end
 
-    # "object_kind"=>"push", 
-    # "before"=>"d0967338b6b719d90e251323acde712d6513dd62", 
-    # "after"=>"a22fecaf3acc70ebbd33326891fd1714bd476216", 
-    # "ref"=>"refs/heads/master", 
+    # "object_kind"=>"push",
+    # "before"=>"d0967338b6b719d90e251323acde712d6513dd62",
+    # "after"=>"a22fecaf3acc70ebbd33326891fd1714bd476216",
+    # "ref"=>"refs/heads/master",
     # "checkout_sha"=>"a22fecaf3acc70ebbd33326891fd1714bd476216",
     # "message"=>nil,
     # "user_id"=>254,
@@ -33,9 +33,14 @@ module Writer
     # "repository"=>{"name"=>"test-book-ii", "url"=>"git@git.zhibimo.com:qiu-liang/test-book-ii.git", "description"=>nil, "homepage"=>"http://git.zhibimo.com/qiu-liang/test-book-ii", "git_http_url"=>"http://git.zhibimo.com/qiu-liang/test-book-ii.git", "git_ssh_url"=>"git@git.zhibimo.com:qiu-liang/test-book-ii.git", "visibility_level"=>0}
 
     # "commits"=>[{"id"=>"a22fecaf3acc70ebbd33326891fd1714bd476216", "message"=>"test\n", "timestamp"=>"2015-07-15T15:50:52+08:00", "url"=>"http://git.zhibimo.com/qiu-liang/test-book-ii/commit/a22fecaf3acc70ebbd33326891fd1714bd476216", "author"=>{"name"=>"hpyhacking", "email"=>"crazyflapjack@gmail.com"}}]
-    
+
     def hook
-      book = Book.find_by_gitlab_id(params['project_id'])
+      if params['project_id']
+        book = Book.find_by_gitlab_id(params['project_id'])
+      else
+        book = Book.find_by_other_git(params["repository"]["ssh_url"])
+      end
+
 
       if book && params['object_kind'] == 'push'
         commit = params['commits'].last
@@ -50,6 +55,9 @@ module Writer
         BuildJob.perform_later(build.id)
       end
 
+      def other_hook
+        puts "caocoacoa"
+      end
       render nothing: true
     end
   end
